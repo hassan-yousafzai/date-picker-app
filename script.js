f1c61db33fce785bec5553c5a25d0c5115cc2432
Hassan <hassanakbar2012@hotmail.com>
2023-12-30 23:17:15 +0000

    by default show todays date on the date picker button and also on the date picker menu show current month and year

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+import { format } from "date-fns"
+
 const datePicker = document.querySelector(".date-picker")
 const datePickerButton = document.querySelector(".date-picker-button")
+const currentMonthYear = document.querySelector(".current-month")
+
+const today = new Date()
+
+datePickerButton.innerText = format(today, "do MMMM yyyy")
+currentMonthYear.innerText = format(today, "MMMM - yyyy")
 
 datePicker.classList.remove("show")
 
