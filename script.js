d67976e081779fdd533c9705804f92e278f3113e
Hassan <hassanakbar2012@hotmail.com>
2023-12-31 18:05:10 +0000

    fix the selected date bug when we click on next and previous month buttons

--- script.js
+++ script.js
@@ -1,46 +1,31 @@
 import {
   format,
-  fromUnixTime,
   getUnixTime,
+  fromUnixTime,
   addMonths,
   subMonths,
-  eachDayOfInterval,
-  startOfMonth,
   startOfWeek,
-  endOfMonth,
+  startOfMonth,
   endOfWeek,
-  getDate,
+  endOfMonth,
+  eachDayOfInterval,
   isSameMonth,
   isSameDay
 } from "date-fns"
 
-const datePicker = document.querySelector(".date-picker")
 const datePickerButton = document.querySelector(".date-picker-button")
+const datePicker = document.querySelector(".date-picker")
 const datePickerHeaderText = document.querySelector(".current-month")
 const previousMonthButton = document.querySelector(".prev-month-button")
 const nextMonthButton = document.querySelector(".next-month-button")
 const dateGrid = document.querySelector(".date-picker-grid-dates")
-
 let currentDate = new Date()
 
-setDate(new Date())
-
-datePickerButton.addEventListener("click", e => {
+datePickerButton.addEventListener("click", () => {
   datePicker.classList.toggle("show")
-
   const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
-  currentDate = selectedDate // show the current selectedDate when user opens datepicker
-  setupDatePicker(currentDate)
-})
-
-nextMonthButton.addEventListener("click", e => {
-  currentDate = addMonths(currentDate, 1)
-  setupDatePicker()
-})
-
-previousMonthButton.addEventListener("click", e => {
-  currentDate = subMonths(currentDate, 1)
-  setupDatePicker()
+  currentDate = selectedDate
+  setupDatePicker(selectedDate)
 })
 
 function setDate(date) {
@@ -57,21 +42,38 @@ function setupDates(selectedDate) {
   const firstWeekStart = startOfWeek(startOfMonth(currentDate))
   const lastWeekEnd = endOfWeek(endOfMonth(currentDate))
   const dates = eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd })
-
-  dateGrid.innerHTML = "" // this is to clear date grid each time we click on the datepicker button as it will append new date grid again and again
+  dateGrid.innerHTML = ""
 
   dates.forEach(date => {
     const dateElement = document.createElement("button")
+    dateElement.classList.add("date")
+    dateElement.innerText = date.getDate()
     if (!isSameMonth(date, currentDate)) {
       dateElement.classList.add("date-picker-other-month-date")
     }
-
     if (isSameDay(date, selectedDate)) {
       dateElement.classList.add("selected")
     }
+    console.log(selectedDate)
+    dateElement.addEventListener("click", () => {
+      setDate(date)
+      datePicker.classList.remove("show")
+    })
 
-    dateElement.classList.add("date")
-    dateElement.innerText = getDate(date)
     dateGrid.appendChild(dateElement)
   })
 }
+
+nextMonthButton.addEventListener("click", () => {
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
+  currentDate = addMonths(currentDate, 1)
+  setupDatePicker(selectedDate)
+})
+
+previousMonthButton.addEventListener("click", () => {
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
+  currentDate = subMonths(currentDate, 1)
+  setupDatePicker(selectedDate)
+})
+
+setDate(new Date())
