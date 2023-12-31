125fd21e634c612561e07d89d5a0c38b73126f83
Hassan <hassanakbar2012@hotmail.com>
2023-12-31 15:47:42 +0000

    show current date on the datePickerButton

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-console.log("hi")
+import { format } from "date-fns"
+
+const today = new Date()
 
 const datePicker = document.querySelector(".date-picker")
 const datePickerButton = document.querySelector(".date-picker-button")
+
+datePickerButton.innerText = format(today, "MMMM do, yyyy")
 
 datePickerButton.addEventListener("click", e => {
   datePicker.classList.toggle("show")
