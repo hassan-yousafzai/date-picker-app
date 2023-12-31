2843a6d1b7175d37ab0abfec33300ed5b544ba8f
Hassan <hassanakbar2012@hotmail.com>
2023-12-31 16:16:44 +0000

    add previous month logic

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-import { format, fromUnixTime, getUnixTime } from "date-fns"
+import { format, fromUnixTime, getUnixTime, subMonths } from "date-fns"
 
 const datePicker = document.querySelector(".date-picker")
 const datePickerButton = document.querySelector(".date-picker-button")
 const datePickerHeaderText = document.querySelector(".current-month")
+const prevMonthButton = document.querySelector(".prev-month-button")
 
 setDate(new Date())
+
+prevMonthButton.addEventListener("click", e => {
+  console.log("prevMonthButton clicked")
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
+
+  setDate(subMonths(selectedDate, 1))
+  setupDatePicker(subMonths(selectedDate, 1))
+})
 
 datePickerButton.addEventListener("click", e => {
   datePicker.classList.toggle("show")
@@ -21,6 +30,4 @@ function setDate(date) {
 
 function setupDatePicker(selectedDate) {
   datePickerHeaderText.innerText = format(selectedDate, "MMMM - yyyy")
-
-  console.log(selectedDate)
 }
