42cd04e6da4daa7003b0c2ea27addc875a5ca100
Hassan <hassanakbar2012@hotmail.com>
2023-12-31 15:44:35 +0000

    on click button toggle the datepicker

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hi")
+
+const datePicker = document.querySelector(".date-picker")
+const datePickerButton = document.querySelector(".date-picker-button")
+
+datePickerButton.addEventListener("click", e => {
+  datePicker.classList.toggle("show")
+})
