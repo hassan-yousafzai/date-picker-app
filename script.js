02a0119eec7f2b9baf84a5ce6402e584e8633dc6
Hassan <hassanakbar2012@hotmail.com>
2023-12-31 15:55:49 +0000

    show current month and year on the datepicker header text

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const today = new Date()
 
 const datePicker = document.querySelector(".date-picker")
 const datePickerButton = document.querySelector(".date-picker-button")
+const datePickerHeaderText = document.querySelector(".current-month")
 
 datePickerButton.innerText = format(today, "MMMM do, yyyy")
 
 datePickerButton.addEventListener("click", e => {
   datePicker.classList.toggle("show")
+  datePickerHeaderText.innerText = format(today, "MMMM - yyyy")
 })
